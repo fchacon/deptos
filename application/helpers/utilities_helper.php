84900a3079529b1aa0eb75c71fdcaef2850e0470
fchacon <felipe.chacon@gmail.com>
2014-05-04 01:01:38 -0400

    Algunas configuraciones

---
+++ application/helpers/utilities_helper.php
@@ -0,0 +1,8 @@
+<?php
+function getJs($file) {
+	return base_url()."js/".$file.".js";
+}
+
+function getCss($file) {
+	return base_url()."css/".$file.".css";
+}
---
+++ application/views/login.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Administración comunidad</title>
+	</head>
+	<body>
+	</body>
+</html>
