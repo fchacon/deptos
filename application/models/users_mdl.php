e4a48f1aa17a0fb428624b9dcef912fe30cff713
Pablo <pcarreno@trabajando.com>
2014-06-09 23:50:43 -0400

    [WS][PHP] Servicio login

--- application/models/users_mdl.php
+++ application/models/users_mdl.php
@@ -1,12 +1,21 @@
 <?php
 class Users_mdl extends CI_Model {
 
+    var $ws_class = "user";
+
 	function __construct() {
 		parent::__construct();
+		$this->load->library('ws');
 	}
 	
 	function validate($email, $password, $format = 'array') {
-		return true;
+		//return true;
+	    $data = array ( 'email' => $email , 'password' => $password  );
+		$result = $this->ws->post($this->ws_class."/logIn",$data);
+		if($format == 'array'){
+			return json_decode($result, true);
+		}else
+			return $result;
 	}
 	
 	function forgottenPassword($email, $format = 'array') {
